8c35f386720c96fa6091b1422d288e56ee1688b1
Juan Sierra Pons <juan@elsotanillo.net>
2014-01-20 09:22:09 +0100

    support for interface selection added

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,9 +14,10 @@ class knockd (
   $package_name = $knockd::params::package_name,
   $service_config = $knockd::params::service_config,
   $service_name = $knockd::params::service_name,
-  $sequence_open = $knockd::params::service_sequence_open,
-  $sequence_close = $knockd::params::service_sequence_close,
-  $port_to_manage = $knockd::params::port_to_manage, 
+  $sequence_open = $knockd::params::sequence_open,
+  $sequence_close = $knockd::params::sequence_close,
+  $port_to_manage = $knockd::params::port_to_manage,
+  $interface_to_manage = $knockd::params::interface_to_manage,
 ) inherits knockd::params {
   anchor {'knockd::start':}->
   class {'knockd::package':}->
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,5 +5,6 @@ class knockd::params {
   $sequence_open = '7000,8000,9000' # example 2222:udp,3333:tcp,4444:udp
   $sequence_close = '9000,8000,7000'
   $port_to_manage = '22'
+  $interface_to_manage = 'eth0'
   $service_default_config = '/etc/default/knockd'
 }
